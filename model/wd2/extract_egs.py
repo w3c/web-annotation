39f3c91c22b9dbc29fc1c20fb4b24ff49dab6a0f
Rob Sanderson <azaroth42@gmail.com>
2016-03-08 14:49:38 -0800

    Add collection example, take out non annotation examples

--- model/wd2/extract_egs.py
+++ model/wd2/extract_egs.py
@@ -32,32 +32,63 @@ egs = dom.xpath('//pre[@class="example highlight"]')
 
 x = 0
 
+# NB this is a string to ensure ordering of the keys
+collection = """
+{
+  "@context": "http://www.w3.org/ns/anno.jsonld",
+  "id": "http://example.org/collection1",
+  "type": "AnnotationCollection",
+  "creator": "http://www.w3.org/",
+  "label": "Annotation Examples",
+  "total": %i,
+  "first": {
+    "id": "http://example.org/collection1/page1",
+    "type": "AnnotationPage",
+    "startIndex": 0,
+    "items": [
+"""
+
+end = """
+    ]
+  }
+}
+"""
+
+annos = []
+
 for eg in egs:
 	egdata = eg.xpath('./text()')[0]
 	if egdata.strip()[0] == '{':
-		x += 1  # Only do this once per example!
 		# JSON-LD
 		try:
-			json.loads(egdata)
+			myjs = json.loads(egdata)
+			if myjs['type'] != "Annotation":
+				continue
+			x += 1  # Only do this once per example!
+			egdata = egdata.strip()
 			fh = file("examples/correct/anno%s.json" % x, 'w')
 			fh.write(egdata)
 			fh.close()
+			eglines = egdata.split('\n')
+			eglines = ["      " + y for y in eglines]
+			egl2 = [eglines[0]]
+			egl2.extend(eglines[2:])
+			egd = '\n'.join(egl2)
+			annos.append(egd)
 		except:
 			print "Busted: " + eg.xpath('@title')[0]
 			print egdata
 
-		# Now generate turtle?
-		if 0:
-			g = Graph()
-			data = pfxstr + "\n\n" + egdata
-			try:
-				g.parse(data=data, format="turtle")
-				fh = file("examples/correct/anno%s.ttl" % x, 'w')
-				fh.write(data)
-				fh.close()
-			except:
-				pass
 	else:	
 		print "Busted: " + eg.xpath('@title')[0]
 		print egdata
 		print "\n\n"
+
+fh = file("examples/correct/collection1.json", 'w')
+fh.write(collection % len(annos))
+for a in annos:
+	fh.write(a)
+	if a != annos[-1]:
+		fh.write(',\n')
+fh.write(end)
+fh.close()

65a9faf39fcae61d23803f3b0077478cb4a6c83d
Rob Sanderson <azaroth42@gmail.com>
2016-05-22 21:09:07 +0200

    undo plane use of template

--- model/wd2/extract_egs.py
+++ model/wd2/extract_egs.py
@@ -1,6 +1,6 @@
 from lxml import etree
 import json
-from rdflib import Graph, Namespace
+OBfrom rdflib import Graph, Namespace
 from rdflib.namespace import NamespaceManager
 
 namespaces = {
@@ -8,7 +8,7 @@ namespaces = {
               'dc' : Namespace('http://purl.org/dc/elements/1.1/'),
               'dcterms' : Namespace('http://purl.org/dc/terms/'),
               'dctypes' : Namespace('http://purl.org/dc/dcmitype/'),
-              'owl' : Namespace('http://www.w3.org/2002/07/owl#'),
+  B            'owl' : Namespace('http://www.w3.org/2002/07/owl#'),
               'rdf' : Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#'),
               'rdfs' : Namespace('http://www.w3.org/2000/01/rdf-schema#'),
               'skos' : Namespace('http://www.w3.org/2004/02/skos/core#'),
@@ -24,7 +24,6 @@ for (key,val) in namespaces.copy().iteritems():
 pfxstr = '\n'.join(pfxs)
 
 fh = file('index-respec.html')
-fh = file('index-nametemplate.html')
 data = fh.read()
 fh.close()
 

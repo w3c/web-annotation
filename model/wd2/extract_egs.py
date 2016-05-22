76177e31a5e7b1c09a0d56e06c93928633138e34
Rob Sanderson <azaroth42@gmail.com>
2016-05-22 22:25:11 +0200

    Remove weird characters from editing in terminal

--- model/wd2/extract_egs.py
+++ model/wd2/extract_egs.py
@@ -1,6 +1,6 @@
 from lxml import etree
 import json
-OBfrom rdflib import Graph, Namespace
+from rdflib import Graph, Namespace
 from rdflib.namespace import NamespaceManager
 
 namespaces = {
@@ -8,7 +8,7 @@ namespaces = {
               'dc' : Namespace('http://purl.org/dc/elements/1.1/'),
               'dcterms' : Namespace('http://purl.org/dc/terms/'),
               'dctypes' : Namespace('http://purl.org/dc/dcmitype/'),
-  B            'owl' : Namespace('http://www.w3.org/2002/07/owl#'),
+              'owl' : Namespace('http://www.w3.org/2002/07/owl#'),
               'rdf' : Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#'),
               'rdfs' : Namespace('http://www.w3.org/2000/01/rdf-schema#'),
               'skos' : Namespace('http://www.w3.org/2004/02/skos/core#'),

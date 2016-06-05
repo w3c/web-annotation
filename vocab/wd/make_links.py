db5d755f97946e2123828b76259779289c236440
Rob Sanderson <azaroth42@gmail.com>
2016-06-05 13:03:36 -0700

    integrate example highlighting/extraction, add paolo's diagrams

--- vocab/wd/make_links.py
+++ vocab/wd/make_links.py
@@ -1,31 +1,39 @@
 
 from lxml import etree
 import re
+import codecs
 
 from pygments import highlight
 from pygments.lexers import TurtleLexer
 from pygments.formatters import HtmlFormatter
-import codecs
+
+from rdflib import Graph, Namespace
+from rdflib.namespace import NamespaceManager
+
+namespaces = {
+              'oa' : Namespace('http://www.w3.org/ns/oa#'),
+              'dc' : Namespace('http://purl.org/dc/elements/1.1/'),
+              'dcterms' : Namespace('http://purl.org/dc/terms/'),
+              'dctypes' : Namespace('http://purl.org/dc/dcmitype/'),
+              'owl' : Namespace('http://www.w3.org/2002/07/owl#'),
+              'rdf' : Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#'),
+              'rdfs' : Namespace('http://www.w3.org/2000/01/rdf-schema#'),
+              'skos' : Namespace('http://www.w3.org/2004/02/skos/core#'),
+              'foaf' : Namespace('http://xmlns.com/foaf/0.1/'),
+           	  'xsd' : Namespace('http://www.w3.org/2001/XMLSchema#'),
+           	  'as' : Namespace('http://www.w3.org/ns/activitystreams#'),
+           	  'schema' : Namespace('http://schema.org/')
+              }
+
+pfxs = []
+
+for (key,val) in namespaces.copy().iteritems():
+	pfxs.append("@prefix %s: <%s> ." % (key, val))
+pfxstr = '\n'.join(pfxs)
+
 
 usre = re.compile("^_(.+)_:(.+)$")
 includere = re.compile("[%][%]include/([^ ]+)[%][%]")
-
-namespaces = {
-    "dc":      "http://purl.org/dc/elements/1.1/",
-    "dcterms": "http://purl.org/dc/terms/",
-    "dctypes": "http://purl.org/dc/dcmitype/",
-    "foaf":    "http://xmlns.com/foaf/0.1/",
-    "rdf":     "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-    "rdfs":    "http://www.w3.org/2000/01/rdf-schema#",
-    "skos":    "http://www.w3.org/2004/02/skos/core#",
-    "xsd":     "http://www.w3.org/2001/XMLSchema#",
-    "ldp":     "http://www.w3.org/ns/ldp#",
-    "iana":    "http://www.iana.org/assignments/relation/",
-    "owl":     "http://www.w3.org/2002/07/owl#",
-    "cnt":     "http://www.w3.org/2011/content#",    
-    "as":      "http://www.w3.org/ns/activitystreams#",
-    "schema":  "http://schema.org/"
-}
 
 fh = codecs.open('index-linktemplate.html', 'r', 'utf-8')
 data = fh.read()
@@ -111,7 +119,6 @@ for tech in techs:
 			newstr = ", ".join(newt)
 			data = data.replace("</strong> %s" % t, "</strong> %s" % newstr, 1)
 
-
 # Reparse to get all of the changes above...
 dom = etree.HTML(data)
 egs = dom.xpath('//pre[@class="example nohighlight"]')
@@ -124,6 +131,9 @@ css = css.replace(".turtle  { background: #f8f8f8; }", "")
 css = "<style>%s</style>" % css
 data = data.replace("<style>", css + "<style>")
 
+
+x=1
+
 for eg in egs:
 	egdata = eg.xpath('./text()')[0]
 	egdata = egdata.strip()
@@ -132,6 +142,20 @@ for eg in egs:
 	if egdata.startswith("GET "):
 		continue
 
+	g = Graph()
+	gdata = pfxstr + "\n\n" + egdata
+	try:
+		g.parse(data=gdata, format="turtle")
+		fh = codecs.open("examples/correct/anno%s.ttl" % x, 'w', 'utf-8')
+		fh.write(data)
+		fh.close()
+	except:	
+		print "Busted: " + eg.xpath('@title')[0]
+		print egdata
+		print "\n\n"
+		raise
+	x += 1
+
 	# Now syntax highlight for turtle
 	eghtml = highlight(egdata, ttl, fmt)	
 	eghtml = eghtml.replace("<pre>", '<pre class="nohighlight">')
@@ -139,7 +163,8 @@ for eg in egs:
 	egdata = egdata.replace(">", "&gt;")
 	data = data.replace(egdata, eghtml, 1)
 
-# Write out the result
+
+# Write out the final result
 fh = codecs.open("index-respec.html", 'w', 'utf-8')
 fh.write(data)
 fh.close()

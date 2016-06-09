e5ca0ace3a3d98bda96e044c24bc25ec14820c31
Ivan Herman <ivan@ivan-herman.net>
2016-06-09 16:11:37 +0200

    Updated the admin part of the repo

--- admin/mkcr.sh
+++ admin/mkcr.sh
@@ -9,7 +9,7 @@ cp -r ../vocab/wd/images TR/annotation-vocab/
 cp ../protocol/wd/index.html TR/annotation-vocab/
 
 cp ../jsonld/anno.jsonld ns/
-cp ../vocab/wd/ontology/index* ns/oa/
+cp ../vocab/wd/ontology/index* ns/
 
 cp ../jsonld/*frame.jsonld annotation/2016/frames/
 cp ../model/wd2/examples/correct/* annotation/2016/examples/json/
